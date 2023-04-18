bea9505017cfb9d3c868b07ed5f47aac97965394
jai <jaijai.stell@gmail.com>
2023-04-18 11:05:56 -0700

    updates article

---
+++ src/components/ArticleList.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Article from "./Article"
+// ! this is the parent of article.js thats why we import it - if you notice Jai, App.js is overacrhing parent so it imports all components
+
+function ArticleList({}){
+    function(
+        
+        <main>{articles}</main>
+    )
+};
+
+export default ArticleList;
