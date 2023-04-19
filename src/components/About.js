09f2c2ed41c38c60cafb18ce518a2b5aa694326b
jai <jaijai.stell@gmail.com>
2023-04-19 15:30:01 -0700

    why does this not pass the testssssss

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,12 @@
 import React from "react";
 
-function About({image = "https://via.placeholder.com/215", about}){
+function About({about, image = "https://via.placeholder.com/215"}) {
     return(
         <aside>
-            {/* {!image || image = " " ? null : <img src="https://via.placeholder.com/215"></img>} */}
-            <img src={image} alt="blog logo"></img>
+            <img src={image} alt="blog logo" />
             <p>{about}</p>
         </aside>
-    )
-};
+    );
+}
 
 export default About;
--- src/components/Article.js
+++ src/components/Article.js
@@ -1,13 +1,32 @@
 import React from "react";
 
-function Article({title, date = "January 1, 1970", preview}){
-    return(
+function Article({ title, date = "January 1, 1970", preview, minutes }) {
+
+    const createEmoji = () => {
+        let interval = 5;
+        let emoji = "☕️";
+        let result = "";
+
+        if (minutes >= 30) {
+            interval = 10;
+            emoji = "🍱";
+        }
+        for (let i = 0; i < minutes; i += interval) {
+            result += emoji;
+        }
+        return result;
+    }
+
+    return (
         <article>
             <h3>{title}</h3>
             <small>{date}</small>
+            <small>
+                •{createEmoji()} {minutes} min read
+            </small>
             <p>{preview}</p>
         </article>
-    )
-};
+    );
+}
 
 export default Article;
